e53b052401ec737e5c90bc1a459a0f230966f3f2
Vasco Coelho <v.coelho@campus.fct.unl.pt>
2018-04-29 16:33:59 +0100

    round 2

--- reddit_schema.sh
+++ reddit_schema.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-cd /apache-cassandra-1.2.16/bin/cassandra-cli
+cd /apache-cassandra-1.2.16/bin/
 
+./cassandra-cli
 create keyspace reddit;
 use reddit;
 create column family permacache with column_type = 'Standard' and comparator = 'BytesType';
